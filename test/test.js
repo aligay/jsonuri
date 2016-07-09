e0e5f8e6a7291f01bb7721ad14d5e3f37630e9f6
linkjun <yanqing.lyq@alibaba-inc.com>
2016-07-09 17:22:14 +0800

    fix walk of path return value.

--- test/test.js
+++ test/test.js
@@ -1,2 +1,20 @@
 var JsonUri = require('../dist/jsonuri.common');
 console.log(JsonUri)
+
+let exObj = {
+  a: 2,
+  b: {
+    b1: {
+      b11: 311,
+      b12: 312
+    },
+    b2: 32
+  }
+};
+
+
+JsonUri.walk(exObj, function(obj, key, raw, path) {
+  console.log(obj,key,raw, path, "入栈")
+})
+
+console.log(exObj);
