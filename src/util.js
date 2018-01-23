416a052b09c772a8ec5989b5065dc0a0dfd197ff
haozi <syntaxright@gmail.com>
2018-01-23 11:39:48 +0800

    fix [walk]

--- src/util.js
+++ src/util.js
@@ -1,42 +1,42 @@
-function noop() {}
+function noop () {}
 
-export function isInteger(num) {
+export function isInteger (num) {
   return Number.isInteger(num)
 }
-export function isObject(val) {
-  return Object.prototype.toString.call(val) === '[object Object]';
+export function isObject (val) {
+  return Object.prototype.toString.call(val) === '[object Object]'
 }
 
-export function isArray(val) {
-  return Object.prototype.toString.call(val) === '[object Array]';
+export function isArray (val) {
+  return Object.prototype.toString.call(val) === '[object Array]'
 }
 
-export function objectForeach(obj, callback) {
+export function objectForeach (obj, callback) {
   let isBreak = false
-  function _break() {
+  function _break () {
     isBreak = true
   }
 
-  for(let prop of Object.keys(obj)) {
-    if(isBreak) break
+  for (let prop of Object.keys(obj)) {
+    if (isBreak) break
     callback(obj[prop], prop, obj, {_break})
   }
 }
 
-export function arrayMove(arr, old_index, new_index) {
+export function arrayMove (arr, old_index, new_index) {
   if (new_index >= arr.length) {
-    var k = new_index - arr.length;
+    var k = new_index - arr.length
     while ((k--) + 1) {
-      arr.push(undefined);
+      arr.push(undefined)
     }
   }
-  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-  return arr;
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0])
+  return arr
 }
 
 const arrPro = Array.prototype
-export function normalizePath(...path) {
-  //path = isArray(path) ? path : [path]
+export function normalizePath (...path) {
+  // path = isArray(path) ? path : [path]
   path = arrPro.concat.apply(arrPro, path).join('/').split('/')
   path = ['/', combingPathKey(path).join('/')].join('')
   if (path !== '/') {
@@ -52,21 +52,29 @@ export function normalizePath(...path) {
  * @param  {[type]} ascentionFn  [description]
  * @return {[type]}              [description]
  */
-export function walk(obj = {}, descentionFn = noop, ascentionFn = noop) {
-  var path = []
-  function _walk(obj) {
+export function walk (obj = {}, descentionFn = noop, ascentionFn = noop) {
+  let walkMap = []
+  let path = []
+  function _walk (obj) {
     objectForeach(obj, (val, key, raw, {_break}) => {
       let isBreak = false
-      function _gBreak() {
+      if (val !== null && typeof val === 'object') {
+        if (walkMap.indexOf(val) > -1) {
+          throw new Error(`obj is not a finite set`)
+        }
+        walkMap.push(val)
+      }
+
+      function _gBreak () {
         _break()
         isBreak = true
-        if(isArray(raw)) {
+        if (isArray(raw)) {
           path.pop()
         }
       }
 
-      if(val === raw) {
-        console.log('Circular-reference: ' + normalizePath(path));
+      if (val === raw) {
+        console.log('Circular-reference: ' + normalizePath(path))
         _break() // break 只会跳出当前一层循环
         return
       }
@@ -76,7 +84,7 @@ export function walk(obj = {}, descentionFn = noop, ascentionFn = noop) {
       path.pop()
       if (val instanceof Object) {
         path.push(key)
-        if(isBreak) return
+        if (isBreak) return
         _walk(val)
         path.pop()
         ascentionFn(val, key, raw, {path: normalizePath(path), _break: _gBreak})
@@ -88,7 +96,7 @@ export function walk(obj = {}, descentionFn = noop, ascentionFn = noop) {
   return _walk(obj)
 }
 
-export function indexOf(path) {
+export function indexOf (path) {
   return path.split('/').filter(item => item).slice(-1)[0] - 0
 }
 
@@ -97,35 +105,34 @@ export function indexOf(path) {
  * @param  {Array} keys  ['','menu','id','','.']
  * @return {Array}       ['menu','id']
  */
-export function combingPathKey(keys) {
+export function combingPathKey (keys) {
 
   // {empty}
   while (~keys.indexOf('')) {
-    var _i = keys.indexOf('');
-    keys.splice(_i, 1);
+    var _i = keys.indexOf('')
+    keys.splice(_i, 1)
   }
 
   // .
   while (~keys.indexOf('.')) {
-    var _i = keys.indexOf('.');
-    keys.splice(_i, 1);
+    var _i = keys.indexOf('.')
+    keys.splice(_i, 1)
   }
 
   // ..
   while (~keys.indexOf('..')) {
-    var _i = keys.indexOf('..');
-    keys[_i] = keys[_i - 1] = null;
-    delete keys[_i];
-    delete keys[_i - 1];
-    keys.splice(_i, 1);
-    keys.splice(_i - 1, 1);
+    let _i = keys.indexOf('..')
+    keys[_i] = keys[_i - 1] = null
+    delete keys[_i]
+    delete keys[_i - 1]
+    keys.splice(_i, 1)
+    keys.splice(_i - 1, 1)
   }
 
-  return keys;
+  return keys
 }
 
-export function findParents(arr1, arr2) {
-  
+export function findParents (arr1, arr2) {
 }
 
 /**
@@ -134,12 +141,11 @@ export function findParents(arr1, arr2) {
  * @return {String} 返回类型
  */
 let __class2types = {}
-objectForeach(['Boolean', 'Number', 'String', 'Function', 'Array', 'Date', 'RegExp', 'Object', 'Error'], function(item, index){
-  __class2types[`[object ${item}]`] = item.toLowerCase();
+objectForeach(['Boolean', 'Number', 'String', 'Function', 'Array', 'Date', 'RegExp', 'Object', 'Error'], function (item, index) {
+  __class2types[`[object ${item}]`] = item.toLowerCase()
 })
-export function getType(obj){
-  if ( obj == null ) return String( obj )
-  return (typeof obj === "object" || typeof obj === "function") ?
-  __class2types[__class2types.toString.call(obj)] || "object" : typeof obj;
+export function getType (obj) {
+  if (obj == null) return String(obj)
+  return (typeof obj === 'object' || typeof obj === 'function') ?
+    __class2types[__class2types.toString.call(obj)] || 'object' : typeof obj
 }
-
