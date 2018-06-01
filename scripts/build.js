edd7bd249a4b2cd7d18d9090d07f09dec8f73997
haozi <syntaxright@gmail.com>
2018-06-01 13:48:26 +0800

    released version 2.0.0

--- scripts/build.js
+++ scripts/build.js
@@ -21,7 +21,7 @@ const path = require('path')
   const htmlPath = path.resolve(__dirname, '../www/index.html')
   let html = fs.readFileSync(htmlPath, 'utf8')
   html = html.replace(/jsonuri@[^/]*\//, `jsonuri@${pkg.version}/`)
-             .replace(/(<a href="\/\/www.npmjs.com\/package\/jsonuri">)(2.0)(<\/a>)/, `$1${pkg.version}$3`)
+             .replace(/(<a href="\/\/www.npmjs.com\/package\/jsonuri">)([^<]*)(<\/a>)/, `$1${pkg.version}$3`)
 
   fs.writeFileSync(htmlPath, html, 'utf8')
 
