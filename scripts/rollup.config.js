8e93113bde770dea89768449535d823acf7d1fba
haozi <syntaxright@gmail.com>
2019-07-16 16:37:43 +0800

    rename

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -2,7 +2,7 @@ const typescript = require('rollup-plugin-typescript2')
 const pkg = require('../package.json')
 const banner = `/*!
 * ${pkg.name} v${pkg.version}
-* (c) ${new Date().getFullYear()} @aligay
+* (c) ${new Date().getFullYear()} @allgay
 * Released under the ${pkg.license} License.
 */`
 
