124f51fc757580abfc0fec2398d7d4b2e6e96c5e
haozi <syntaxright@gmail.com>
2019-07-16 16:26:06 +0800

    upgrade pre-publish

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -2,6 +2,7 @@ const version = require('../package.json').version
 const util = require('./util')
 util.sh(`
   git tag publish/${version} &&
-  git push origin publish/${version}
+  git push origin publish/${version} &&
+  npm run deploy:gh-pages
 `)
 console.log(version)
