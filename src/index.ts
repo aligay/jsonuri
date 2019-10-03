91333e49e42a5235afe2fa09f8e6ee59733d5fa2
haozi <syntaxright@gmail.com>
2019-10-03 23:01:01 +0800

    refactor: 💡 refactor

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,14 @@
-import get from './methods/get'
-import set from './methods/set'
+export { default as get } from './methods/get'
+export { default as set } from './methods/set'
 
-import rm from './methods/rm'
-import swap from './methods/swap'
-import mv from './methods/mv'
-import insert from './methods/insert'
-import { up, down } from './methods/upDown'
+export { default as rm } from './methods/rm'
+export { default as swap } from './methods/swap'
+export { default as mv } from './methods/mv'
+export { default as insert } from './methods/insert'
+export { up, down } from './methods/upDown'
 
-import normalizePath from './methods/normalizePath'
-import _computePath from './methods/_computePath'
+export { default as normalizePath } from './methods/normalizePath'
+export { default as _computePath } from './methods/_computePath'
 
-import isCircular from './methods/isCircular'
-import walk from './methods/walk'
-
-export {
-  get,
-  set,
-
-  rm,
-  swap,
-  mv,
-  insert,
-  up,
-  down,
-
-  walk,
-  normalizePath,
-  _computePath,
-
-  isCircular
-}
+export { default as isCircular } from './methods/isCircular'
+export { default as walk } from './methods/walk'
